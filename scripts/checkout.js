33a77e757f2b1bd33b51e877a6aea8bb53124fb3
humhliao <humhliao@gmail.com>
2024-09-04 15:20:57 +0700

    14k

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -44,10 +44,11 @@ cart.forEach((cartItem) => {
               data-product-id="${matchingProduct.id}">
               Update
             </span>
-            <input class="quantity-input">
-            <span class="save-quantity-link link-primary
-            js-save-link" data-product-id=${matchingProduct.id}>
-            Save</span>
+            <input class="quantity-input js-quantity-input-${matchingProduct.id}">
+            <span class="save-quantity-link link-primary js-save-link"
+              data-product-id="${matchingProduct.id}">
+              Save
+            </span>
             <span class="delete-quantity-link link-primary js-delete-link" data-product-id="${matchingProduct.id}">
               Delete
             </span>
@@ -142,7 +143,7 @@ document.querySelectorAll('.js-update-link')
     });
   });
 
-  document.querySelectorAll('.js-save-link')
+document.querySelectorAll('.js-save-link')
   .forEach((link) => {
     link.addEventListener('click', () => {
       const productId = link.dataset.productId;
@@ -151,5 +152,10 @@ document.querySelectorAll('.js-update-link')
         `.js-cart-item-container-${productId}`
       );
       container.classList.remove('is-editing-quantity');
+
+      const quantityInput = document.querySelector(
+        `.js-quantity-input-${productId}`
+      );
+      const newQuantity = Number(quantityInput.value);
     });
   });
